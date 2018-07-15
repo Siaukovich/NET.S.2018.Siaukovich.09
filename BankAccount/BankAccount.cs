f46c4dc7a6e23de68a97ad2809b41a73f9224b98
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-15 16:21:18 +0300

    Holder class and draft of Bank account class were created.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿namespace BankAccount
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Class for saving bank account info.
+    /// </summary>
+    public sealed class BankAccount
+    {
+        #region Private fields
+
+        private string _number;
+
+        #endregion
+
+        #region Propereties
+
+        /// <summary>
+        /// Gets the balance.
+        /// </summary>
+        public decimal Balance { get; private set; }
+
+        /// <summary>
+        /// Gets the bonus points.
+        /// </summary>
+        public int BonusPoints { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the holder of this account.
+        /// </summary>
+        public Holder Holder { get; set; }
+
+        /// <summary>
+        /// Gets this bank account number.
+        /// Bank account number is 28 digits and letters.
+        /// </summary>
+        public string Number
+        {
+            get => this._number;
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                const string PATTERN = @"^[\d|A-Z]{28}$";
+                var regex = new Regex(PATTERN);
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Bank account number must be a 28 sybol long string that contains digits and uppercase letters.");
+                }
+
+                this._number = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BankAccount"/> class.
+        /// </summary>
+        /// <param name="holder">
+        /// Bank account holder.
+        /// </param>
+        /// <param name="accountNumber">
+        /// Bank account number.
+        /// </param>
+        public BankAccount(Holder holder, string accountNumber)
+        {
+            this.Holder = holder;
+            this.Number = accountNumber;
+        }
+    }
+}
---
+++ BankAccount/Holder.cs
@@ -0,0 +1,129 @@
+﻿namespace BankAccount
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Class that describes a single customer by his name, contact phone and revenue.
+    /// </summary>
+    public class Holder
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Customer's name.
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// Customer's contact phone.
+        /// </summary>
+        private string contactPhone;
+
+        /// <summary>
+        /// Customer's revenue.
+        /// </summary>
+        private string homeAdress;
+
+        #endregion
+
+        #region Propereties
+
+        /// <summary>
+        /// Gets or sets current holder's name.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if passed name is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown if passed name doesn't satisfy given rules:
+        /// 1. Name can be only in English.
+        /// 2. Name must contain at least two words. 
+        /// 3. All words separated by one whitespace.
+        /// 4. Only first letter of each word must be uppercase.
+        /// </exception>
+        public string Name
+        {
+            get => this.name;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                const string PATTERN = @"^([A-Z][a-z]* )+[A-Z][a-z]*$";
+                var regex = new Regex(PATTERN);
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Name must be in English, contain at least two words separated by " + 
+                                                "single whitespace and only first letter of each name must be uppercase.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets current holders's contact phone.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if passed number is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown if passed number not in form "+X (XXX) XXX-XXXX".
+        /// </exception>
+        public string ContactPhone
+        {
+            get => this.contactPhone;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                const string PATTERN = @"^\+\d \(\d{3}\) \d{3}-\d{4}";
+                var regex = new Regex(PATTERN);
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Number must be in form \"+X (XXX) XXX-XXXX\".");
+                }
+
+                this.contactPhone = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets current customer's revenue.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Thrown if passed value is negative or contain more than 2 decimal places.
+        /// </exception>
+        public string HomeAdress
+        {
+            get => this.homeAdress;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                const string PATTERN = @"^\d+ [A-Z][a-z]* st\.$";
+                var regex = new Regex(PATTERN);
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Adress must be in form \"house-numer street-name st.\".");
+                }
+
+                this.homeAdress = value;
+            }
+        }
+
+        #endregion
+    }
+}
