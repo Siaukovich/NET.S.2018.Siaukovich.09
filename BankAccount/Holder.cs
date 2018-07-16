01fda2152fdc54352d8ac2e868e4eebe729f31e1
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-16 20:17:39 +0300

    Holder class was refactored.

--- BankAccount/Holder.cs
+++ BankAccount/Holder.cs
@@ -4,26 +4,31 @@
     using System.Text.RegularExpressions;
 
     /// <summary>
-    /// Class that describes a single customer by his name, contact phone and revenue.
+    /// Class that describes a single holder by his name, contact phone, email and home address.
     /// </summary>
     public class Holder
     {
         #region Private fields
 
         /// <summary>
-        /// Customer's name.
+        /// Holder's name.
         /// </summary>
         private string name;
 
         /// <summary>
-        /// Customer's contact phone.
+        /// Holder's contact phone.
         /// </summary>
         private string contactPhone;
 
         /// <summary>
-        /// Customer's revenue.
+        /// Holder's revenue.
         /// </summary>
-        private string homeAdress;
+        private string homeAddress;
+
+        /// <summary>
+        /// Holder's email.
+        /// </summary>
+        private string email;
 
         #endregion
 
@@ -66,7 +71,7 @@
         }
 
         /// <summary>
-        /// Gets or sets current holders's contact phone.
+        /// Gets or sets current holder's contact phone.
         /// </summary>
         /// <exception cref="ArgumentNullException">
         /// Thrown if passed number is null.
@@ -80,47 +85,92 @@
 
             set
             {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
-
-                const string PATTERN = @"^\+\d \(\d{3}\) \d{3}-\d{4}";
-                var regex = new Regex(PATTERN);
-                if (!regex.IsMatch(value))
-                {
-                    throw new ArgumentException("Number must be in form \"+X (XXX) XXX-XXXX\".");
-                }
+                ThrowForInvalidString(
+                    value: value,
+                    regexpPattern: @"^\+\d \(\d{3}\) \d{3}-\d{4}$",
+                    errorMessage: "Number must be in form \"+X (XXX) XXX-XXXX\"."
+                );
 
                 this.contactPhone = value;
             }
         }
 
         /// <summary>
-        /// Gets or sets current customer's revenue.
+        /// Gets or sets current customer's home address.
         /// </summary>
-        /// <exception cref="ArgumentException">
-        /// Thrown if passed value is negative or contain more than 2 decimal places.
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if passed home address is null.
         /// </exception>
-        public string HomeAdress
+        /// <exception cref="ArgumentException">
+        /// Thrown if passed home address not in form "house-number street-name st.".
+        /// </exception>
+        public string HomeAddress
         {
-            get => this.homeAdress;
+            get => this.homeAddress;
 
             set
             {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
+                ThrowForInvalidString(
+                    value: value,
+                    regexpPattern: @"^\d+ [A-Z][a-z]* st\.$",
+                    errorMessage: "Adress must be in form \"house-number street-name st.\"."
+                );
 
-                const string PATTERN = @"^\d+ [A-Z][a-z]* st\.$";
-                var regex = new Regex(PATTERN);
-                if (!regex.IsMatch(value))
-                {
-                    throw new ArgumentException("Adress must be in form \"house-numer street-name st.\".");
-                }
+                this.homeAddress = value;
+            }
+        }
 
-                this.homeAdress = value;
+        /// <summary>
+        /// Gets or sets current customer's email.
+        /// </summary>
+        public string Email
+        {
+            get => this.email;
+            set
+            {
+                ThrowForInvalidString(
+                    value: value, 
+                    regexpPattern: @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$", 
+                    errorMessage: "Wrong email format."
+                );
+
+                this.homeAddress = value;
+            }
+        }
+
+        #endregion
+
+        #region Private helpers
+
+        /// <summary>
+        /// Checks passed value string for null and for matching provided regexp pattern.
+        /// </summary>
+        /// <param name="value">
+        /// Value that needs to be checked.
+        /// </param>
+        /// <param name="regexpPattern">
+        /// Regexp pattern that validates passed value.
+        /// </param>
+        /// <param name="errorMessage">
+        /// Error message for ArgumentException.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if passed value is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown if passed value doesn't match provided regexp pattern.
+        /// </exception>
+        private static void ThrowForInvalidString(string value, string regexpPattern, string errorMessage)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var regex = new Regex(regexpPattern);
+            if (!regex.IsMatch(value))
+            {
+                throw new ArgumentException(errorMessage);
             }
         }
 
